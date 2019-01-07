fe1a326dabda4d7a675023f9d651e76ca16cb664
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-01-07 16:11:00 -0500

    Delete upper.py

--- upper.py
+++
@@ -1,7 +0,0 @@
-# This program accepts a string from the user and prints the string in uppercase with 3 exclamation points
-
-#input string
-user_string = input("Enter your phrase here: ")
-
-#print capitalized string and exclamation points
-print (user_string.upper() + "!!!")
