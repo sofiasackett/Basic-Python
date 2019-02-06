4897fa4e926ed380b1907b186138373a0495b418
Sofia Sackett <43033791+sofiasackett@users.noreply.github.com>
2019-02-05 19:56:11 -0500

    Delete strings.py

--- strings.py
+++
@@ -1,7 +0,0 @@
-#This program uses string slicing and concatenation to transform the statement 'I do not attend college'
-
-#input given statement
-statement = "I do not attend college"
-
-#print modified statement
-print(statement[0:4], statement[9:] + " in Albany")
